eb481b03d674a3d51e448fa27d2d4a163e211559
Al Dm <d44.a1@yandex.ru>
2025-02-12 22:34:00 +0300

    1

---
+++ fonts/css/js/data.js
@@ -0,0 +1,42 @@
+const initialOverlayData = [
+  {
+    image: "../../images/Hero/Hero_8.png",
+    text: "orem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Labore orem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Laboreorem ipsum dolor sit amet consectetur adipisicing elit.Impedit vel expedita a modi, officia est cumque. Labore",
+  },
+  {
+    image: "../../images/Hero/Hero_7.png",
+    text: "reeeeerr",
+  },
+  {
+    image: "../../images/Hero/Hero_3.png",
+    text: "rjjjjjjjr",
+  },
+  {
+    image: "../../images/Hero/Hero_1.png",
+    text: "vvvvvvvr",
+  },
+  {
+    image: "../../images/Hero/Hero_2.png",
+    text: "rbbbbbbbbbjr",
+  },
+  {
+    image: "../../images/Hero/Hero_6.png",
+    text: "rbbbbbbbbbjr",
+  },
+  {
+    //ПОЛЬЗОВАТЕЛЬСКОЕ СОГЛАШЕНИЕ
+    image: "../../images/Hero/Hero_5.png",
+    text: "пользовательское",
+  },
+  {
+    //ПОЛИТИКА КОНФИДЕНЦИАЛЬНОСТИ
+    image: "../../images/Hero/Hero_5.png",
+    text: "политика",
+  },
+  {
+    image: "../../images/Hero/Hero_4.png",
+    text: "rjjooooooojr",
+  },
+];
+
+export { initialOverlayData };
---
+++ fonts/css/js/index.js
@@ -0,0 +1,97 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const contactsName = document.querySelector(".header__contacts-name");
+  const phoneLinks = document.querySelectorAll(".header__contacts-phones li a");
+  const initialText = contactsName.textContent;
+
+  phoneLinks.forEach(function (link) {
+    link.addEventListener("mouseover", function (event) {
+      const name = event.target.getAttribute("data-name");
+      if (name) {
+        contactsName.textContent = name;
+      }
+    });
+    link.addEventListener("mouseout", function () {
+      contactsName.textContent = initialText;
+    });
+  });
+});
+
+//меняем цвет на адресе,реквизитах,банках
+document.addEventListener("DOMContentLoaded", function () {
+  const lists = document.querySelectorAll(".requisites-hover");
+
+  lists.forEach((list) => {
+    list.addEventListener("mouseover", () => {
+      lists.forEach((l) => l.classList.add("active"));
+    });
+    list.addEventListener("mouseout", () => {
+      lists.forEach((l) => l.classList.remove("active"));
+    });
+  });
+});
+
+//открытие оверлея и отрисовка темплейта
+const overlay = document.getElementById("overlay");
+const overlayClose = document.querySelector(".overlay-close");
+const overlayInner = document.querySelector(".overlay-inner");
+const body = document.body; // Получаем элемент body
+
+document.addEventListener("DOMContentLoaded", function () {
+  const openOverlayBtns = document.querySelectorAll("[id]"); // Выбираем все элементы с id
+
+  if (openOverlayBtns && overlay && overlayClose && overlayInner) {
+    openOverlayBtns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const templateClassName = btn.id; // Берем ID элемента, на который кликнули, и используем как КЛАСС для поиска темплейта.
+        const template = document.querySelector(
+          `template.${templateClassName}`
+        ); // Ищем template с таким же КЛАССОМ
+
+        if (template && template.tagName === "TEMPLATE") {
+          // Проверяем, что template существует и это действительно элемент TEMPLATE
+          // Клонируем содержимое template
+          const templateContent = template.content.cloneNode(true);
+
+          // Очищаем содержимое overlay-inner
+          overlayInner.innerHTML = "";
+
+          // Добавляем содержимое template в overlay-inner
+          overlayInner.appendChild(templateContent);
+
+          // Возвращаем прокрутку overlay-inner в начало (с задержкой)
+          setTimeout(() => {
+            overlayInner.scrollTo(0, 0);
+          }, 0); // Небольшая задержка
+
+          // Добавляем класс 'lock' к body
+          body.classList.add("lock");
+
+          // Отображаем оверлей
+          overlay.classList.add("active");
+        } else {
+          console.warn(
+            `Template with class "${templateClassName}" not found or is not a <template> element.`
+          );
+        }
+      });
+    });
+
+    overlayClose.addEventListener("click", () => {
+      overlay.classList.remove("active");
+      // Удаляем класс 'lock' из body
+      body.classList.remove("lock");
+    });
+
+    overlay.addEventListener("click", function (event) {
+      if (event.target === overlay) {
+        overlay.classList.remove("active");
+        // Удаляем класс 'lock' из body
+        body.classList.remove("lock");
+      }
+    });
+  } else {
+    console.error(
+      "Необходимые элементы не найдены (overlay, overlayClose, overlayInner)."
+    );
+  }
+});
